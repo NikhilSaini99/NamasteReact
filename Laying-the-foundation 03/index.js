5031aa50c69712d5d128dab0eb2c20a3b70c5bf2
Nikhil Saini <nikhilsaini42@gmail.com>
2023-09-09 04:42:11 +0530

    comment added in episode 3

--- Laying-the-foundation 03/index.js
+++ Laying-the-foundation 03/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 //JSX - HTML LIKE FILE, BUT NOT HTML
 //JSX CODE IS TRANSPILE before going to browser so that browser can understand.. this translation is done by (BABEL)
-
+//Functional Components returns us JSX
 const JSXHeading = () => (
   <h1 className="heading" tabIndex="5">
     Namaste React using JSX
@@ -22,5 +22,5 @@ const HeadingComponent = () => (
 );
 
 console.log(HeadingComponent);
-console.log(<HeadingComponent />);
+console.log(<HeadingComponent />);                                                     
 export default HeadingComponent;
